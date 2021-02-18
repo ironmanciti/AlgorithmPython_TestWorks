3d4ce7172e5e96928b540694fe58b05497cd6608
YJOh <trimurti@naver.com>
2021-02-18 13:55:49 +0900

    first:

---
+++ 40_sample_setup/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='module_test',
+    version="1.0.0",
+    description="test packaging", 
+    author="YJOh",
+    author_email="test@gmail.com",
+    url="www.test.com",
+    py_modules=['module_test']
+)
